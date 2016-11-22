7b6d7387ab2d0acbef95a6555e16badd34d29ef0
dominic.lee <dominic.lee@kakaocorp.com>
2016-11-22 15:01:36 +0900

    toastr!

--- to-be-frontend/app/components/view/View.jsx
+++ to-be-frontend/app/components/view/View.jsx
@@ -1,14 +1,27 @@
 import React  from 'react';
+import toastr from 'toastr';
 
 export default class View extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  _click = () => {
+    toastr.info('Are you the 6 fingered man?');
+    toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!');
+    toastr.success('Have fun storming the castle!', 'Miracle Max Says');
+    toastr.error('I do not think that word means what you think it means.', 'Inconceivable!');
+    toastr.success('We do have the Kapua suite available.', 'Turtle Bay Resort', {timeOut: 5000});
+    // toastr.remove();
+    // toastr.clear();
+  }
+
   render() {
     return (
       <div className="row c-m-t">
-        view~~
+        <button className="btn btn-sm btn-outline-secondary" onClick={this._click}>
+          click
+        </button>
       </div>
     );
   }
